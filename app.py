6d7885defd2f52c05966196ee2b050e6b613d766
Troy Lopez <troy.n.lopez@gmail.com>
2024-05-28 18:06:45 -0700

    covers basic test cases

--- app.py
+++ app.py
@@ -12,30 +12,35 @@ db_pth = "../users" # path to users on remote
 
 def hash(user, passw):
     salt = secrets.token_bytes(16)
-    prep = lambda user, passw, sal: (user + passw + salt .encode())
-    return salt, hashlib.sha256(prep(user, passw, salt ).hexdigest())
+    prep = lambda user, passw, sal: ((user + passw ).encode() + sal)
+    encoded_prep = prep(user, passw, salt)
+    hashed = (hashlib.sha256(encoded_prep)).hexdigest()
+    return salt, hashed
+
 
 
 def user_exists(user):
     with open(db_pth, 'r') as file:
-        data = json.loads(file)
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            data = {}
 
         if user in data:
-            return 1
-        else: return 0
+            return True
+        else:
+            return False
 
 
 @app.route('/register', methods=['POST'])
 def register():
     data = request.json
-    print(data)
     if 'user' not in data:
         return jsonify({'Error': 'User field is missing'}), 400
     if 'pass' not in data:
-        return jsonify({'Error: password not in data'}), 400
+        return jsonify({'Error': 'Password field is missing'}), 400
     if user_exists(data['user']):
-        # return conflict status
-        return jsonify({'Error: user already exists'}, 409)
+        return jsonify({'Error': 'User already exists'}), 409
     
     else:
         salt, tok = hash(data['user'], data['pass'])
@@ -46,18 +51,17 @@ def register():
         with open(db_pth, 'r+') as file:
             try:
                 db_data = json.load(file)
-            # if blank
             except json.JSONDecodeError:
                 db_data = {}
             
             db_data[data['user']] = new_user
-            file.seek(0)  # Move the cursor to the beginning of the file
-            json.dump(db_data, file)  # Write the updated data back to the file
-            file.truncate()  # Remove any remaining data
+            file.seek(0)
+            json.dump(db_data, file)
+            file.truncate()
             
         return jsonify({'Message': 'User registered successfully'}), 201
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
     #add port=##### to change the port the api uses
